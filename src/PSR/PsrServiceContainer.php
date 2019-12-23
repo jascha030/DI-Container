38ef6a6a5324b17cefff8400c2db490e967a2987
Jascha van Aalst <mr.jascha@gmail.com>
2019-12-23 12:02:53 +0100

    Added SET method

--- src/PSR/PsrServiceContainer.php
+++ src/PSR/PsrServiceContainer.php
@@ -2,11 +2,25 @@
 
 namespace Jascha030\DIC\Psr;
 
+use Exception;
 use Psr\Container\ContainerInterface;
 
 class PsrServiceContainer implements ContainerInterface
 {
     protected $instances = [];
+
+    public function set($abstract, $concrete = null)
+    {
+        if (!class_exists($abstract)) {
+            throw new Exception("Class {$abstract} could not be found");
+        }
+
+        if (!$concrete) {
+            $concrete = $abstract;
+        }
+
+        $this->instances[$abstract] = $concrete;
+    }
 
     public function get($id)
     {
