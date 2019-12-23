7153c4b86dfc9fe5b228f4dd56ae26c6d104cb85
Jascha van Aalst <mr.jascha@gmail.com>
2019-12-23 12:25:17 +0100

    Added  exception classes.

---
+++ src/Exception/ClassNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jascha030\DIC\Exception;
+
+use Exception;
+
+class ClassNotFoundException extends Exception
+{
+}
---
+++ src/Exception/Dependency/UnresolvableDependencyException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jascha030\DIC\Exception\Dependency;
+
+use Exception;
+
+class UnresolvableDependencyException extends Exception
+{
+}
